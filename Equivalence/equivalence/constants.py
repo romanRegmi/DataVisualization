85d9cb276da3de54fd6f4470c219877ea85429ed
Roman Regmi <romanregmi@hotmail.com>
2024-01-12 19:06:52 +0545

    Add files via upload

---
+++ Equivalence/equivalence/constants.py
@@ -0,0 +1,4 @@
+BASE_URL = 'https://tucdc.edu.np/student'
+EMAIL = 'sudeepdahal75@gmail.com'
+PWD = 'Tucdc123@'
+
---
+++ Equivalence/main.py
@@ -0,0 +1,8 @@
+from equivalence.equivalence import Equivalence
+
+if __name__=='__main__':
+    inst = Equivalence()
+    inst.land_first_page()
+    inst.login()
+    inst.apply()
+    inst.selectUni()
