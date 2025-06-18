6fab41ddeca711e0139a7aa1d2e9065fde6fe3d5
Roman Regmi <roman.regmi@docsumo.com>
2025-06-18 20:12:31 +0545

    Deleted Fiels

--- Equivalence/equivalence/constants.py
+++
@@ -1,4 +0,0 @@
-BASE_URL = 'https://tucdc.edu.np/student'
-EMAIL = ''
-PWD = ''
-
--- Equivalence/main.py
+++
@@ -1,8 +0,0 @@
-from equivalence.equivalence import Equivalence
-
-if __name__=='__main__':
-    inst = Equivalence()
-    inst.land_first_page()
-    inst.login()
-    inst.apply()
-    inst.selectUni()
